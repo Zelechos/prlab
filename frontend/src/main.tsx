f5e1ec8df22b3b2b085601d15d8e4365ae614388
Alex T.H. <zelechos@hotmail.com>
2025-03-17 01:00:54 -0300

    refactor(v0.1.2): Refactor & add NavBar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import AppElectron from './AppElectron.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App />
-      <AppElectron />
   </StrictMode>,
 )
 
